a4c6a3595d73b548ecb7e71dbef76d82eb7b556a
Lincy Akoth Odhiambo <122862647+Ninolincy@users.noreply.github.com>
2024-01-08 00:24:48 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/pup
-# install flask from pip3
-
-package {'flask':
-	ensure 	=> '2.1.0',
-	provider => 'pip3';
-	}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# kill the process killmenow
-exec {'pkill':
-	command => 'pkill killmenow',
-	path 	=> '/usr/bin',
-	}
