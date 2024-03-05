c1c015e2a6a45308c71a23f719662ad606d1719e
Ninolincy <lincy3akoth@gmail.com>
2024-03-05 18:00:13 +0300

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'My User Agent 1.0'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')
