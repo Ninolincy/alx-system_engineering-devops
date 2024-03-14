4d6e0898b26aa388c631b89410ebdadd64fbcf76
Ninolincy <lincy3akoth@gmail.com>
2024-03-14 20:31:10 +0300

    Webstack-debugging#4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# User limit
+
+exec { 'Change soft limit':
+    command => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+    provider=> shell,
+}
+
+exec { 'Change hard limit':
+    command => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
+    provider=> shell,
+}
